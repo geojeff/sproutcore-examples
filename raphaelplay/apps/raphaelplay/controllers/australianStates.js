240c3ba4a4676d5bb4626b4b785cf9bc574748bd
Jeff Pittman <geojeff@jeff-pittmans-macbook-pro.local>
2010-05-08 07:08:49 -0500

    cleaned up; added nowShowing function

--- raphaelplay/apps/raphaelplay/controllers/australianStates.js
+++ raphaelplay/apps/raphaelplay/controllers/australianStates.js
@@ -1,62 +1,17 @@
 // ==========================================================================
 // Project:   RaphaelPlay.australianStatesController
-// Copyright: ©2010 My Company, Inc.
 // ==========================================================================
 /*globals RaphaelPlay */
 
 /** @class
 
-  (Document Your Controller Here)
+  An ArrayController for the Australian states.
 
   @extends SC.ArrayController
 */
 RaphaelPlay.australianStatesController = SC.ArrayController.create(
-//SC.CollectionViewDelegate,
 /** @scope RaphaelPlay.australianStatesController.prototype */
 {
-    //selection: null,
-
-    listEnabled: YES,
-
-    raphaelView: null,
-
-    listView: null,
-
-    /**
-     *     Note: This is a hack. Currently the SC.CollectionView does not correctly respond to
-     *         changes made to its isEnabled property. Therefore we have to force the collection
-     *             view to reload to make sure the view and its list item views correctly receive the
-     *                 changes. 
-     *
-     *                     In addition, the SC.CollectionView does not handle changes to its isSelectable
-     *                         property either. Sigh.
-     *
-     *                             Such is life working with a beta release. [Sept 6, 2009]
-     *                               */
-    listEnabledChanged: function() {
-        var list = this.get('listView');
-        list.set('isEnabled', this.get('listEnabled'));
-        list.reload();
-    }.observes('listEnabled'),
-
-    highlightRaphaelPath:  function(p) {
-        p.animate({ fill: "#333", stroke: "#666" }, 500);
-        p.animate({ fill: st.color, stroke: "#ccc" }, 500);
-        p.toFront();
-        r.safari();
-    },
-
-    summary: function() {
-        var len = this.get('length'),
-        ret;
-
-        if (len && len > 0) {
-            ret = len === 1 ? "1 australianState": "%@ australianStates".fmt(len);
-        } else ret = "No australianStates";
-
-        return ret;
-    }.property('length').cacheable(),
-
     selectAustralianState: function(australianState) {
         SC.RunLoop.begin();
         var selectedState = RaphaelPlay.store.find(RaphaelPlay.australianState, australianState);
@@ -64,10 +19,14 @@ RaphaelPlay.australianStatesController = SC.ArrayController.create(
         SC.RunLoop.end();
     },
 
-    unselectAustralianState: function(australianState) {
+    unselectAustralianState: function() {
         SC.RunLoop.begin();
         this.selectObject(null);
         SC.RunLoop.end();
     },
+
+    nowShowing: function() {
+        return this.get('hasSelection') ? 'summaryView' : 'helpView'
+    }.property('hasSelection').cacheable()
 
 });
