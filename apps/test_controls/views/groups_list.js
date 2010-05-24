1f6ec028cebf7aa13284b71b0edb64f107e55060
Jeff Pittman <geojeff@jeff-pittmans-macbook-pro.local>
2010-05-24 14:49:02 -0500

    added Sproutcore headers to .js files in responders and views

--- apps/test_controls/views/groups_list.js
+++ apps/test_controls/views/groups_list.js
@@ -1,7 +1,11 @@
 // ==========================================================================                                                                                                                                                                                             
-//   Project:   test_controls
+// Project:   SproutCore - JavaScript Application Framework
+// Copyright: ©2006-2010 Sprout Systems, Inc. and contributors.
+//            Portions ©2008-2010 Apple Inc. All rights reserved.
+// License:   Licensed under MIT license (see license.js)
 // ==========================================================================
 /*globals TestControls*/
+
 TestControls.GroupsList = SC.ScrollView.extend({
     topToolbar: SC.NavigationBarView.design({
         layout: { top: 0, left: 0, right: 0, height: 32 },
