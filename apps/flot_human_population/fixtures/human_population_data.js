d7e87ef91da4d379e70abf04f7df6ee58f0ca048
Jeff Pittman <geojeff@jeff-pittmans-macbook-pro.local>
2010-06-02 13:59:08 -0500

    removed incorrect guid

--- apps/flot_human_population/fixtures/human_population_data.js
+++ apps/flot_human_population/fixtures/human_population_data.js
@@ -3,11 +3,11 @@
 sc_require('models/human_population_data');
 
 FlotHumanPopulation.human_population_data.FIXTURES = [
-    { 'guid': 'wo',  'timestamp':  -631130400000, 'year': 1950, 'world': 2519, 'africa': 221, 'asia': 1398, 'europe': 547, 'latin_america': 167, 'northern_america': 172, 'oceania': 13 },
-    { 'guid': 'af',  'timestamp':  -473364000000, 'year': 1955, 'world': 2756, 'africa': 247, 'asia': 1542, 'europe': 575, 'latin_america': 191, 'northern_america': 187, 'oceania': 14 },
-    { 'guid': 'as',  'timestamp':  -315597600000, 'year': 1960, 'world': 2982, 'africa': 277, 'asia': 1674, 'europe': 601, 'latin_america': 209, 'northern_america': 204, 'oceania': 16 },
-    { 'guid': 'eu',  'timestamp':  -157744800000, 'year': 1965, 'world': 3335, 'africa': 314, 'asia': 1899, 'europe': 634, 'latin_america': 250, 'northern_america': 219, 'oceania': 18 },
-    { 'guid': 'la',  'timestamp':       21600000, 'year': 1970, 'world': 3692, 'africa': 357, 'asia': 2143, 'europe': 656, 'latin_america': 285, 'northern_america': 232, 'oceania': 19 },
-    { 'guid': 'na',  'timestamp':   157788000000, 'year': 1975, 'world': 4068, 'africa': 408, 'asia': 2397, 'europe': 675, 'latin_america': 322, 'northern_america': 243, 'oceania': 21 },
-    { 'guid': 'oc',  'timestamp':   315554400000, 'year': 1980, 'world': 4435, 'africa': 470, 'asia': 2632, 'europe': 692, 'latin_america': 361, 'northern_america': 256, 'oceania': 23 },
+    { 'timestamp':  -631130400000, 'year': 1950, 'world': 2519, 'africa': 221, 'asia': 1398, 'europe': 547, 'latin_america': 167, 'northern_america': 172, 'oceania': 13 },
+    { 'timestamp':  -473364000000, 'year': 1955, 'world': 2756, 'africa': 247, 'asia': 1542, 'europe': 575, 'latin_america': 191, 'northern_america': 187, 'oceania': 14 },
+    { 'timestamp':  -315597600000, 'year': 1960, 'world': 2982, 'africa': 277, 'asia': 1674, 'europe': 601, 'latin_america': 209, 'northern_america': 204, 'oceania': 16 },
+    { 'timestamp':  -157744800000, 'year': 1965, 'world': 3335, 'africa': 314, 'asia': 1899, 'europe': 634, 'latin_america': 250, 'northern_america': 219, 'oceania': 18 },
+    { 'timestamp':       21600000, 'year': 1970, 'world': 3692, 'africa': 357, 'asia': 2143, 'europe': 656, 'latin_america': 285, 'northern_america': 232, 'oceania': 19 },
+    { 'timestamp':   157788000000, 'year': 1975, 'world': 4068, 'africa': 408, 'asia': 2397, 'europe': 675, 'latin_america': 322, 'northern_america': 243, 'oceania': 21 },
+    { 'timestamp':   315554400000, 'year': 1980, 'world': 4435, 'africa': 470, 'asia': 2632, 'europe': 692, 'latin_america': 361, 'northern_america': 256, 'oceania': 23 },
     ]
