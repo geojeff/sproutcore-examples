385aa434bf44d992e35c953c025b3d81c110af56
Jeff Pittman <geojeff@jeff-pittmans-macbook-pro.local>
2010-06-07 21:32:53 -0500

    removed dumpProps utility, saving locally for use later

--- apps/flot_oil_spills/controllers/spill.js
+++ apps/flot_oil_spills/controllers/spill.js
@@ -16,26 +16,7 @@ FlotOilSpills.spillController = SC.ArrayController.create(
         var selectedSpill = this.get('content').objectAt(spill_item.dataIndex);
         this.selectObject(selectedSpill);
         FlotOilSpills.mainPage.mainPane.spills.contentView.scrollToContentIndex(spill_item.dataIndex);
-        //debugger;
-        console.error(spill_item.series, spill_item.datapoint);
         plot.highlight(spill_item.series, spill_item.datapoint);
-        //this.dumpProps(plot);
-    },
-
-    dumpProps: function(obj, parent) {
-        for (var i in obj) {
-            // if a parent (2nd parameter) was passed in, then use that to 
-            // build the message. Message includes i (the object's property name) 
-            // then the object's property value on a new line 
-            if (parent) { var msg = parent + "." + i + "\n" + obj[i]; } else { var msg = i + "\n" + obj[i]; }
-            // Display the message. If the user clicks "OK", then continue. If they 
-            // click "CANCEL" then quit this level of recursion 
-            if (!confirm(msg)) { return; }
-            // If this property (i) is an object, then recursively process the object 
-            if (typeof obj[i] == "object") { 
-                if (parent) { this.dumpProps(obj[i], parent + "." + i); } else { this.dumpProps(obj[i], i); }
-            }
-        }
     },
 });
 
