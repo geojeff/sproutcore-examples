8f72eaae7bf0744dbe16c7b0a17ba8234ffa0d2e
Jeff Pittman <geojeff@jeff-pittmans-macbook-pro.local>
2010-06-11 11:16:44 -0500

    added main view and checkboxes for mockup of a mapping system

--- apps/svg_editor/resources/main_page.js
+++ apps/svg_editor/resources/main_page.js
@@ -9,13 +9,67 @@
 
 ScalableVectorGraphicsEditor.mainPage = SC.Page.design({
     mainPane: SC.MainPane.design({
-        childViews: 'editor'.w(),
+        childViews: 'map_system'.w(),
 
-        editor: SC.WebView.extend(SC.ContentDisplay, {
-            layout: { width: 750, height: 650 },
-            //value: 'http://svg-edit.googlecode.com/svn/tags/stable/editor/svg-editor.html',
-            value: 'http://www.geojeff.com/svg-edit-read-only/editor/svg-editor.html',
-        }),
+        map_system: SC.View.design({
+            layout: { width: 1200, height: 650 },
+            childViews: 'editor map_controls'.w(),
 
+            editor: SC.WebView.extend(SC.ContentDisplay, {
+                layout: { width: 750, height: 650 },
+                value: 'http://www.geojeff.com/svg-edit-read-only/editor/svg-editor.html',
+                mapDataDidChange: function(){}
+            }),
+            map_controls: SC.View.design({
+                layout: { left: 720, top: 40 },
+                childViews: "form".w(),
+                form: SC.FormView.design({
+                    classNames: ["svg_editor"],
+                    layout: { left: 38, right: 0, bottom: 20 },
+                    childViews: "header roads demography streams water_features vegetation political_boundaries national_parks".w(),
+
+                    header: SC.LabelView.design({
+                        layout: { width: 100, height: 24 },
+                        classNames: "header".w(),
+                        value: "Map Layers:"
+                    }),
+                    roads: SC.FormView.row(SC.CheckboxView.design({
+                        layout: {width: 150, height: 24},
+                        controlSize: SC.SMALL_CONTROL_SIZE,
+                        valueBinding: 'ScalableVectorGraphicsEditor.mapController.isRoadsDataShowing'
+                    })),
+                    demography: SC.FormView.row(SC.CheckboxView.design({
+                        layout: {width: 150, height: 24},
+                        controlSize: SC.SMALL_CONTROL_SIZE,
+                        valueBinding: 'ScalableVectorGraphicsEditor.mapController.isDemographyDataShowing'
+                    })),
+                    streams: SC.FormView.row(SC.CheckboxView.design({
+                        layout: {width: 150, height: 24},
+                        controlSize: SC.SMALL_CONTROL_SIZE,
+                        valueBinding: 'ScalableVectorGraphicsEditor.mapController.isStreamsDataShowing',
+                    })),
+                    water_features: SC.FormView.row(SC.CheckboxView.design({
+                        layout: {width: 150, height: 24},
+                        controlSize: SC.SMALL_CONTROL_SIZE,
+                        valueBinding: 'ScalableVectorGraphicsEditor.mapController.isWaterFeaturesDataShowing',
+                    })),
+                    vegetation: SC.FormView.row(SC.CheckboxView.design({
+                        layout: {width: 150, height: 24},
+                        controlSize: SC.SMALL_CONTROL_SIZE,
+                        valueBinding: 'ScalableVectorGraphicsEditor.mapController.isVegetationDataShowing'
+                    })),
+                    political_boundaries: SC.FormView.row(SC.CheckboxView.design({
+                        layout: {width: 150, height: 24},
+                        controlSize: SC.SMALL_CONTROL_SIZE,
+                      valueBinding: 'ScalableVectorGraphicsEditor.mapController.isPoliticalBoundariesDataShowing'
+                    })),
+                    national_parks: SC.FormView.row(SC.CheckboxView.design({
+                        layout: {width: 150, height: 24},
+                        controlSize: SC.SMALL_CONTROL_SIZE,
+                        valueBinding: 'ScalableVectorGraphicsEditor.mapController.isNationalParksDataShowing'
+                    }))
+                })
+            })
+        })
     })
 });
